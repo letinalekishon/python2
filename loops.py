787a25b90cac7860ea344619100032eec47089f4
letina <letinaalexlekishon05@gmail.com>
2025-05-28 16:47:27 +0300

    loops.py

---
+++ loops.py
@@ -0,0 +1,13 @@
+fruits = ("banana","apple","mellon","orange")
+
+for i in fruits:
+    print(i)
+
+    num =list(range(1,101))
+    print(num)
+
+
+num=list(range(20,40))
+for i in range(20,40):
+    print(i)
+    
